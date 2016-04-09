c54fe5f048cd2a5630ffdc281ebf2fb288c2d585
Devraj Mehta <devm33@gmail.com>
2016-04-09 17:18:41 -0400

    soln for small case

--- fractiles/ans_small.py
+++ fractiles/ans_small.py
@@ -5,7 +5,10 @@ def choose_tiles(K, C, S):
     """For the small problem S=K so we can always just check the first K tiles."""
     if S < K:
         return 'IMPOSSIBLE'
-    return ' '.join(range(1, S+1))
+    return str_arr(range(1, S+1))
+
+def str_arr(arr):
+    return ' '.join(map(str, arr))
 
 def main():
     T = int(raw_input())
