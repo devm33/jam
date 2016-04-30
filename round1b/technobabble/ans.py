759420d429e20798a8602e9d712f9a696d8e7995
Devraj Mehta <devm33@gmail.com>
2016-04-30 13:01:24 -0400

    works on sample cases

---
+++ round1b/technobabble/ans.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+def group_by(pairs, index):
+    group = {}
+    for pair in pairs:
+        cur = pair[index]
+        if cur in group:
+            group[cur] += 1
+        else:
+            group[cur] = 1
+    return group
+
+def real(N, pairs):
+    firsts = group_by(pairs, 0)
+    seconds = group_by(pairs, 1)
+    fakes = 0
+    for pair in pairs:
+        if firsts[pair[0]] > 1 and seconds[pair[1]] > 1:
+            fakes += 1
+            firsts[pair[0]] -= 1
+            seconds[pair[1]] -= 1
+    return fakes
+
+
+def main():
+    T = int(raw_input())
+    for i in range(T):
+        N = int(raw_input())
+        words = [raw_input().split() for i in range(N)]
+        print 'Case #%s: %s' % ((i+1), real(N, words))
+
+
+if __name__ == "__main__":
+    main()
